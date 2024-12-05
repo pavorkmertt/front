7f4ec2e92a3e4e1b4cb543e94731e6a1fede6678
pavorkmertt <marikpetrov2015@gmail.com>
2024-12-05 23:18:54 +0300

    Update api.ts

--- app_init/shared/api/api.ts
+++ app_init/shared/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url =  process.env.API_URL ?? "http://localhost:3002";
+const url =  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3002";
 
 export const $api = axios.create({
     baseURL: url,
