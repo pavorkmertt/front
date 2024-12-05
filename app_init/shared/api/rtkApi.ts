712951f92faa8d7712c4c2f95ac57ca391216d79
pavorkmertt <marikpetrov2015@gmail.com>
2024-12-05 23:32:30 +0300

    Update rtkApi.ts

--- app_init/shared/api/rtkApi.ts
+++ app_init/shared/api/rtkApi.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const url =  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3002";
 
